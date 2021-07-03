9c61af8fe4194090ef5e45b25779b0afdc58efb4
Duarte <al222010329@gmail.com>
2021-07-03 14:28:11 -0500

    Cree un script para instalar y ejecutar lampp en linux

---
+++ iniciar_servicios.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo Estás apunto de iniciar los servicios de lampp
+   cd /opt/lampp
+  chmod +x manager-linux-x64.run
+  sudo ./manager-linux-x64.run
---
+++ instalar_lampp_linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo Estás apunto de instalar lampp
+cd Descargas
+ls -l xampp-linux-x64-8.0.7-0-installer.run
+chmod +x xampp-linux-x64-8.0.7-0-installer.run
+ls -l xampp-linux-x64-8.0.7-0-installer.run
+sudo ./xampp-linux-x64-8.0.7-0-installer.run
